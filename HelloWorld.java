a212e67a39e67d51dc6543476697deec33550c44
JorenzJake <augisjorenz@gmail.com>
2024-10-04 15:38:12 +0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,35 @@
+
+
+import java.util.Scanner; 
+
+public class HelloWorld {
+    public static void main(String[] args) {
+          System.out.print("hi");
+          
+    }
+     
+  
+       
+    
+      
+
+
+
+
+
+
+
+
+     
+    
+
+        
+
+    
+        
+
+    
+         
+    }
+
+}
